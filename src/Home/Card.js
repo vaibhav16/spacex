18ba1b4dbe94e5fe3bfd2c14b9d9f26f59b56205
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2021-04-20 00:12:30 +0530

    design changes

--- src/Home/Card.js
+++ src/Home/Card.js
@@ -1,39 +1,43 @@
-function Card(){
+function Card() {
 
-    return(
-        <div className="px-4 py-3">
-            <div className="row">
-            <div className="col-12 bg-white br-16 fs-14 p-3">
-                <div className="row img-center">
-                <img style={{height:145}} src={"https://images2.imgbox.com/3d/86/cnu0pan8_o.png"}/>
-                </div>
-              
-                <div className="row fwb py-1">
-                    <div className="col-12" style={{color:"blue"}}>DemoSat #2</div>
-                </div>
-                <div className="row py-1">
-                    <div className="col-8 fwb">Mission Ids:</div>
-                    <div className="col-8 offset-1 bullet">Mission Ids</div>
+    return (
+
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-3 col-xl-3">
+            <div className="px-3 py-3  ">
+                <div className="row">
+                    <div className="col-12 bg-white br-16 fs-14 p-3">
+                        <div className="row img-center">
+                            <img style={{ height: 120 }} src={"https://images2.imgbox.com/3d/86/cnu0pan8_o.png"} />
+                        </div>
+
+                        <div className="row fwb py-1">
+                            <div className="col-12" style={{ color: "blue" }}>DemoSat #2</div>
+                        </div>
+                        <div className="row py-1">
+                            <div className="col-8 fwb">Mission Ids:</div>
+                            <div className="col-8 offset-1 bullet">Mission Ids</div>
+                        </div>
+
+                        <div className="row py-1">
+                            <div className="col-6 fwb">Launch Year:</div>
+                            <div className="col-6">2009</div>
+                        </div>
+
+                        <div className="row py-1">
+                            <div className="col-6 fwb">Successful Launch:</div>
+                            <div className="col-6">Launch Success</div>
+                        </div>
+
+                        <div className="row py-1">
+                            <div className="col-6 fwb">Successful Landing:</div>
+                            <div className="col-6">Launch Landing</div>
+                        </div>
+                    </div>
                 </div>
 
-                <div className="row py-1">
-                    <div className="col-6 fwb">Launch Year:</div>
-                    <div className="col-6">2009</div>
-                </div>
-
-                <div className="row py-1">
-                    <div className="col-6 fwb">Successful Launch:</div>
-                    <div className="col-6">Launch Success</div>
-                </div>
-
-                <div className="row py-1">
-                    <div className="col-6 fwb">Successful Landing:</div>
-                    <div className="col-6">Launch Landing</div>
-                </div>
             </div>
-            </div>
-  
         </div>
+
     )
 }
 
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,7 +12,9 @@ function Home() {
             .then(res => {
                 //setData(res);
                 console.log(res);
-                setCards(res.data.map((item) => { return <Card key={Math.random()} data={item} /> }))
+                document.getElementById('loader').style.display = 'none'
+                setCards(res.data.map((item) => { return <Card key={Math.random()} data={item} /> }));
+
             })
             .catch(err => {
                 console.log(err);
@@ -23,93 +25,118 @@ function Home() {
 
 
     return (
-        <div className="container">
+        <React.Fragment>
+            {cards != '' ? generateDOM(cards) : ''}
+
+        </React.Fragment>
+    )
+}
+
+
+function generateDOM(cards) {
+    console.log(cards)
+    return (
+        <div className="container-fluid">
             <div className="row">
                 <div className="col-12">
                     <h1 className="fwb" style={{ fontSize: "2rem" }}>SpaceX Launch Programs</h1>
                 </div>
             </div>
-            <div className="row fs-16 m-0 px-2 py-3">
-                <div className="col-12 pb-4 bg-white br-16">
-                    <div className="row fwb" style={{ fontSize: "22px", marginTop: "10px" }}>
-                        <div className="col-12"> Filters</div>
-                    </div>
-                    <div className="row tc"><div className="col-12 fs-18">Launch Year</div></div>
-                    <div style={{marginTop:"-10px"}} className="mx-4"><hr /></div>
-                    
-                    <div className="row tc py-1">
-                        <div className="col-6">
-                            <button className="year-btn">2006</button></div>
-                        <div className="col-6">  <button className="year-btn">2007</button></div>
-                    </div>
+            <div className="row">
+                <div className="filter col-xs-12 col-sm-12 col-md-6 col-lg-3 col-xl-3">
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2008</button></div>
-                        <div className="col-6">  <button className="year-btn">2009</button></div>
-                    </div>
+                    <div className="row fs-16 m-0 px-2 py-3">
+                        <div className="col-12 pb-4 bg-white br-16">
+                            <div className="row fwb" style={{ fontSize: "22px", marginTop: "10px" }}>
+                                <div className="col-12"> Filters</div>
+                            </div>
+                            <div className="row tc"><div className="col-12 fs-18">Launch Year</div></div>
+                            <div style={{ marginTop: "-10px" }} className="mx-4"><hr /></div>
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2010</button></div>
-                        <div className="col-6">  <button className="year-btn">2011</button></div></div>
+                            <div className="row tc py-1">
+                                <div className="col-6">
+                                    <button className="year-btn">2006</button></div>
+                                <div className="col-6">  <button className="year-btn">2007</button></div>
+                            </div>
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2012</button></div>
-                        <div className="col-6">  <button className="year-btn">2013</button></div></div>
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2008</button></div>
+                                <div className="col-6">  <button className="year-btn">2009</button></div>
+                            </div>
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2014</button></div>
-                        <div className="col-6">  <button className="year-btn">2015</button></div></div>
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2010</button></div>
+                                <div className="col-6">  <button className="year-btn">2011</button></div></div>
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2016</button></div>
-                        <div className="col-6">  <button className="year-btn">2017</button></div></div>
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2012</button></div>
+                                <div className="col-6">  <button className="year-btn">2013</button></div></div>
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2018</button></div>
-                        <div className="col-6">  <button className="year-btn">2019</button></div></div>
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2014</button></div>
+                                <div className="col-6">  <button className="year-btn">2015</button></div></div>
 
-                    <div className="row tc py-2">
-                        <div className="col-6">  <button className="year-btn">2020</button></div>
-                    </div>
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2016</button></div>
+                                <div className="col-6">  <button className="year-btn">2017</button></div></div>
 
-                    <div className="p-2">
-                        <div className="row tc">
-                            <div className="col-12">
-                                <div className="row">
-                                    <div className="col-12 tc fs-18">Successful Launch</div></div>
-                                    <div  style={{marginTop:"-10px"}} className="mx-4"><hr /></div>
-                                <div className="row">
-                                    <div className="col-6"> <button className="year-btn">True</button></div>
-                                    <div className="col-6"> <button className="year-btn">False</button></div>
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2018</button></div>
+                                <div className="col-6">  <button className="year-btn">2019</button></div></div>
+
+                            <div className="row tc py-2">
+                                <div className="col-6">  <button className="year-btn">2020</button></div>
+                            </div>
+
+                            <div className="p-2">
+                                <div className="row tc">
+                                    <div className="col-12">
+                                        <div className="row">
+                                            <div className="col-12 tc fs-18">Successful Launch</div></div>
+                                        <div style={{ marginTop: "-10px" }} className="mx-4"><hr /></div>
+                                        <div className="row">
+                                            <div className="col-6"> <button className="year-btn">True</button></div>
+                                            <div className="col-6"> <button className="year-btn">False</button></div>
+                                        </div>
+                                    </div>
                                 </div>
+
+                            </div>
+
+                            <div className="p-2">
+                                <div className="row tc">
+                                    <div className="col-12">
+                                        <div className="row">
+                                            <div className="col-12 tc fs-18">Successful Landing</div>
+                                        </div>
+                                        <div style={{ marginTop: "-10px" }} className="mx-4"><hr /></div>
+                                        <div className="row">
+                                            <div className="col-6"> <button className="year-btn">True</button></div>
+                                            <div className="col-6"> <button className="year-btn">False</button></div>
+                                        </div>
+                                    </div>
+                                </div>
+
                             </div>
                         </div>
-
                     </div>
 
-                    <div className="p-2">
-                        <div className="row tc">
-                            <div className="col-12">
-                                <div className="row">
-                                    <div className="col-12 tc fs-18">Successful Landing</div>
-                                </div>
-                                <div style={{marginTop:"-10px"}} className="mx-4"><hr /></div>
-                                <div className="row">
-                                <div className="col-6"> <button className="year-btn">True</button></div>
-                                    <div className="col-6"> <button className="year-btn">False</button></div>
-                                </div>
-                            </div>
-                        </div>
-
+                </div>
+                <div className="col-xs-12 col-sm-12 col-md-6 col-lg-9 col-xl-9 offset-xs-12 offset-sm-12 offset-md-6 offset-lg-3 offset-xl-3">
+                    <div className="row">
+                        {cards ? cards : ''}
                     </div>
                 </div>
+
+
+
             </div>
 
-
-
-
-
-            {cards != '' ? cards : ''}
+            <div className="row tc" style={{fontSize:"24px"}}>
+                <div className="col-12">
+                <b>Developed By:</b> Vaibhav Sharma
+                </div>
+            </div>
         </div>
     )
 }
