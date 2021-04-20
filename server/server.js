7af9fa5288bd3d6275d0130fdfade97766a5a92d
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2021-04-20 19:33:26 +0530

    wip

--- server/server.js
+++ server/server.js
@@ -1,28 +1,41 @@
 import express from 'express';
+import http from 'http';
 import fs from 'fs';
 import path from 'path';
 import React from 'react';
-import ReactDomServer from 'react-dom/server';
+var ReactDOMServer = require('react-dom/server');
+import { StaticRouter } from 'react-router-dom';
 import App from '../src/App';
 
 const app = express();
 
 const PORT = 8000;
 
-app.use('^/$',(req,res,next)=>{
-    fs.readFile(path.resolve('./build/index.html'),'utf-8',(err,data)=>{
-        if(err){
+
+app.use('^/$', (req, res, next) => {
+    fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
+        if (err) {
             console.log(err);
             return res.status(500).send("Some Error Happened");
         }
+
+    
+        const context = {};
+        
+        const html = ReactDOMServer.renderToString(
+            <StaticRouter location={req.url} context={context}>
+              <App />
+            </StaticRouter>);
+
+      
         return res.send(data.replace('<div id="root"></div>',
-        `<div id="root">${ReactDomServer.renderToString(<App/>)}</div>`))
+            `<div id="root">${html}</div>`))
     })
 })
 
-app.use(express.static(path.resolve(__dirname,'..','build')))
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log(`App Launched on ${PORT}`)
 })
