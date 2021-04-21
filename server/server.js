18442206f89221de99cb20c2c9375539e48284d7
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2021-04-22 00:45:32 +0530

    wip

--- server/server.js
+++ server/server.js
@@ -7,20 +7,38 @@ var ReactDOMServer = require('react-dom/server');
 import { StaticRouter } from 'react-router-dom';
 import App from '../src/App';
 var expressStaticGzip = require("express-static-gzip");
-
+var compression = require('compression')
 const app = express();
 
 const PORT = 8000;
 
-app.use("/", expressStaticGzip("build",{
-    enableBrotli: true,
-    customCompressions: [{
-        encodingName: 'deflate',
-        fileExtension: 'zz'
-    }],
-    orderPreference: ['br']
-}));
+// For each request for .js file
+// return the compressed version .gz
+// Path to build directory
+const clientDirPath = path.resolve(__dirname, 'build');
+app.get('*.js', function (req, res, next) {
+    const pathToGzipFile = req.url + '.gz';
+    try {
+      // Check if .gz file exists
+      if (fs.existsSync(path.join(clientDirPath, pathToGzipFile))) {
+        // Change the requested .js to return
+        // the compressed version - filename.js.gz
+        req.url = req.url + '.gz';
+        // Tell the browser the file is compressed and it should decompress it.
+        // You will get a blank screen without this header because it will try to parse
+        // the compressed file.
+        res.set('Content-Encoding', 'gzip');
+        res.set('Content-Type', 'text/javascript');
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  
+    next();
+  });
 
+app.use(compression());
+  
 app.use('^/$', (req, res, next) => {
     fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
         if (err) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,5 @@
 var webpack = require('webpack');
-//var CompressionPlugin = require('compression-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
-const BrotliPlugin = require('brotli-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
- 
 
 
 module.exports = {
@@ -25,23 +20,9 @@ module.exports = {
     }]
   },
   plugins: [
-    // new UglifyJsPlugin(),
-    // new CleanWebpackPlugin(['build']),
-    // new webpack.DefinePlugin({
-    //   'process.env.NODE_ENV': JSON.stringify('production')
-    // }),
     new CompressionPlugin({
-      asset: '[path].gz[query]',
       algorithm: 'gzip',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.7
-    }),
-    new BrotliPlugin({
-      asset: '[path].br[query]',
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.7
+      test: /.js$|.css$/,
     })
   ],
   optimization: {
