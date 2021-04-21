bfe84e3eb85466d1d696f506fff1d00aa6bf6d0e
Vaibhav Sharma <vaibhav.sharma@revv.co.in>
2021-04-21 12:46:46 +0530

    minor tweaks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Home from './Home/Home';
 import React from 'react';
 
 function App() {
 
   return (
-
-    <Router>
-      <Switch>
-        <Route path="/" component={Home} />
-      </Switch>
-      
-    </Router>
-
+    <Route path="/" component={Home} />
   );
 }
 
